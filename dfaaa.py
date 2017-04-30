3e8380a8a73d82956b3e4e4ff117375552bca2d4
Yusuke Shinyama <yusuke@shinyama.jp>
2017-04-30 12:27:23 +0900

    Added: Scope and Variable.

--- dfaaa.py
+++ dfaaa.py
@@ -3,7 +3,18 @@ import sys
 from xml.etree.cElementTree import Element
 from xml.etree.cElementTree import ElementTree
 
-class Block:
+class Variable:
+
+    def __init__(self, scope, name, type):
+        self.scope = scope
+        self.name = name
+        self.type = type
+        return
+
+    def __repr__(self):
+        return '<%s(%s)>' % (self.name, self.type)
+
+class Scope:
 
     def __init__(self, parent=None):
         self.parent = parent
@@ -11,18 +22,18 @@ class Block:
         self.children = []
         return
 
-    def add(self, name, type):
-        self.vars[name] = type
+    def add(self, var):
+        self.vars[var.name] = var
         return
 
     def descend(self):
-        c = Block(self)
+        c = Scope(self)
         self.children.append(c)
         return c
 
     def dump(self, indent=''):
-        for (name,type) in self.vars.items():
-            print(indent+('%s: %s' % (name,type)))
+        for var in self.vars.values():
+            print(indent+str(var))
         for c in self.children:
             c.dump(indent+'  ')
         return
@@ -35,25 +46,29 @@ def get_decl(elem):
     name = get_name(elem)
     return (type, name)
 
-def process_block(b, elem):
+def process_block(scope, elem):
     for declstmt in elem.findall('decl_stmt'):
         (param_type, param_name) = get_decl(declstmt.find('decl'))
-        b.add(param_name, param_type)
-    for block in elem.findall('block'):
-        process_block(b.descend(), block)
+        var = Variable(scope, param_name, param_type)
+        scope.add(var)
+    for stmt in elem.getchildren():
+        if stmt.tag == 'if':
+            then = stmt.find('then')
+            process_block(scope.descend(), then.find('block'))
     return
 
 def process_func(elem):
-    b = Block()
+    scope = Scope()
     func_type = get_name(elem.find('type'))
     func_name = get_name(elem)
     params = elem.find('parameter_list')
     for param in params.findall('parameter'):
         (param_type, param_name) = get_decl(param.find('decl'))
-        b.add(param_name, param_type)
+        var = Variable(scope, param_name, param_type)
+        scope.add(var)
     block = elem.find('block')
-    process_block(b, block)
-    b.dump()
+    process_block(scope, block)
+    scope.dump()
     return
 
 def process_root(elem):
--- samples/Foo.java
+++ samples/Foo.java
@@ -3,7 +3,8 @@ public class Foo {
     public static void foo(int x, int y) {
 	int z = x+1;
 	if (z < y) {
-	    x += z;
+	    int a = z;
+	    x += a;
 	}
 	return y*z;
     }
