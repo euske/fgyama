c7a8c3957c93d904c620311f7e89f7e4520fca45
Yusuke Shinyama <yusuke@shinyama.jp>
2017-05-08 06:11:06 +0900

    Produces an incomplete graph.

--- dfaaa.py
+++ dfaaa.py
@@ -3,6 +3,82 @@ import sys
 from xml.etree.cElementTree import Element
 from xml.etree.cElementTree import ElementTree
 
+class Node:
+
+    nid_base = 0
+
+    def __init__(self, elem):
+        Node.nid_base += 1
+        self.nid = Node.nid_base
+        self.elem = elem
+        self.send = []
+        self.recv = []
+        return
+
+    def __repr__(self):
+        return '<%s>' % self.name()
+
+    def name(self):
+        return 'Node_%d' % (self.nid)
+
+    def connect(self, node):
+        if self is node: return
+        #print ('connect', self, node)
+        self.send.append(node)
+        node.recv.append(self)
+        return
+
+class ArgNode(Node):
+
+    def name(self):
+        return 'ArgNode_%d' % (self.nid)
+
+class ReturnNode(Node):
+
+    def name(self):
+        return 'ReturnNode_%d' % (self.nid)
+
+class VarNode(Node):
+
+    def __init__(self, elem, var):
+        Node.__init__(self, elem)
+        self.var = var
+        return
+    
+    def name(self):
+        return 'VarNode_%d_%s' % (self.nid, self.var.name)
+
+class ValueNode(Node):
+
+    def __init__(self, elem, value, type):
+        Node.__init__(self, elem)
+        self.value = value
+        self.type = type
+        return
+    
+    def name(self):
+        return 'ValueNode_%d_%s_%s' % (self.nid, self.value, self.type)
+
+class OpNode(Node):
+
+    OPNAME = {
+        '+': 'add',
+        '-': 'sub',
+        '*': 'mul',
+        '/': 'div',
+        '%': 'mod',
+        '=': 'eq',
+        }
+    
+    def __init__(self, elem, op):
+        Node.__init__(self, elem)
+        self.op = op
+        return
+    
+    def name(self):
+        op = '_'.join( self.OPNAME[c] for c in self.op )
+        return 'OpNode_%d_%s' % (self.nid, op)
+    
 class Variable:
 
     def __init__(self, scope, name, type):
@@ -19,42 +95,113 @@ class Scope:
     def __init__(self, parent=None):
         self.parent = parent
         self.vars = {}
-        self.children = []
         return
 
-    def add(self, var):
-        self.vars[var.name] = var
-        return
+    def bind(self, name, type):
+        var = Variable(self, name, type)
+        self.vars[name] = var
+        return var
 
-    def descend(self):
-        c = Scope(self)
-        self.children.append(c)
-        return c
+    def lookup(self, name):
+        if name in self.vars:
+            return self.vars[name]
+        elif self.parent:
+            return self.parent.lookup(name)
+        else:
+            raise KeyError(name)
 
-    def dump(self, indent=''):
-        for var in self.vars.values():
-            print(indent+str(var))
-        for c in self.children:
-            c.dump(indent+'  ')
-        return
+    def pop(self):
+        return self.vars.values()
 
 def get_name(elem):
     return elem.find('name').text
 
 def get_decl(elem):
-    type = get_name(elem.find('type'))
     name = get_name(elem)
-    return (type, name)
+    type = get_name(elem.find('type'))
+    return (name, type)
 
-def process_block(scope, elem):
-    for declstmt in elem.findall('decl_stmt'):
-        (param_type, param_name) = get_decl(declstmt.find('decl'))
-        var = Variable(scope, param_name, param_type)
-        scope.add(var)
+def process_expr(scope, bindings, elem):
+    vals = []
+    ops = []
+    ins = {}
+    for e in elem.getchildren():
+        if e.tag == 'name':
+            var = scope.lookup(e.text)
+            node = bindings[var]
+            ins[var] = node
+            vals.append(node)
+        elif e.tag == 'literal':
+            (name, type) = (e.text, e.get('type'))
+            node = ValueNode(e, name, type)
+            vals.append(node)
+        elif e.tag == 'operator':
+            ops.append(e.text)
+    if not ops:
+        assert len(vals) == 1
+        return (ins, vals[0])
+    out = OpNode(elem, ''.join(ops))
+    for src in vals:
+        src.connect(out)
+    return (ins, out)
+
+def process_block(parent, bindings, elem):
+    scope = Scope(parent)
+    bindings = bindings.copy()
     for stmt in elem.getchildren():
-        if stmt.tag == 'if':
+        if stmt.tag == 'decl_stmt':
+            decl = stmt.find('decl')
+            (param_name, param_type) = get_decl(decl)
+            var = scope.bind(param_name, param_type)
+            init = decl.find('init')
+            if init:
+                name = decl.find('name')
+                expr = init.find('expr')
+                (ins1, out) = process_expr(scope, bindings, expr)
+                for (v0, dst) in ins1.items():
+                    src = bindings[v0]
+                    src.connect(dst)
+                dst = VarNode(name, var)
+                out.connect(dst)
+                bindings[var] = dst
+                
+        elif stmt.tag == 'expr_stmt':
+            expr = stmt.find('expr')
+            (ins1, out) = process_expr(scope, bindings, expr)
+            for (v0, dst) in ins1.items():
+                src = bindings[v0]
+                src.connect(dst)
+            name = expr.find('name')
+            var = scope.lookup(name.text)
+            dst = VarNode(name, var)
+            out.connect(dst)
+            bindings[var] = dst
+            
+        elif stmt.tag == 'return':
+            expr = stmt.find('expr')
+            (ins1, out) = process_expr(scope, bindings, expr)
+            for (v0, dst) in ins1.items():
+                src = bindings[v0]
+                src.connect(dst)
+            dst = ReturnNode(expr)
+            out.connect(dst)
+            
+        elif stmt.tag == 'if':
             then = stmt.find('then')
-            process_block(scope.descend(), then.find('block'))
+            process_block(scope, bindings, then.find('block'))
+            
+    for var in scope.pop():
+        del bindings[var]
+    return
+
+def show_graph(node, visited):
+    if node in visited: return
+    visited.add(node)
+    for c in node.send:
+        print (' %s -> %s;' % (node.name(), c.name()))
+        show_graph(c, visited)
+    for c in node.recv:
+        show_graph(c, visited)
     return
 
 def process_func(elem):
@@ -62,13 +209,17 @@ def process_func(elem):
     func_type = get_name(elem.find('type'))
     func_name = get_name(elem)
     params = elem.find('parameter_list')
+    bindings = {}
     for param in params.findall('parameter'):
-        (param_type, param_name) = get_decl(param.find('decl'))
-        var = Variable(scope, param_name, param_type)
-        scope.add(var)
+        (param_name, param_type) = get_decl(param.find('decl'))
+        var = scope.bind(param_name, param_type)
+        bindings[var] = ArgNode(param)
     block = elem.find('block')
-    process_block(scope, block)
-    scope.dump()
+    process_block(scope, bindings, block)
+
+    visited = set()
+    for node in bindings.values():
+        show_graph(node, visited)
     return
 
 def process_root(elem):
@@ -90,6 +241,7 @@ def main(argv):
         if k == '-d': debug += 1
     if not args:
         args.append(None)
+    print('digraph G {')
     for path in args:
         if path is not None:
             fp = open(path)
@@ -99,6 +251,7 @@ def main(argv):
         if path is not None:
             fp.close()
         process_root(root)
+    print('}')
     return 0
 
 if __name__ == '__main__': sys.exit(main(sys.argv))
--- samples/f1.java
+++ samples/f1.java
@@ -1,7 +1,8 @@
 public class Foo {
     
     public static void f1(int x, int y) {
-	int z = x+y;
+	int z = x+1;
+        z *= y;
 	return z;
     }
     
