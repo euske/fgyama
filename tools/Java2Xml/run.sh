4d9728ad6f7043328c29e46455d438e87f06a46c
Yusuke Shinyama <yusuke@shinyama.jp>
2017-07-09 11:47:36 +0900

    Moved: Java2Xml to src.

--- tools/Java2Xml/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-JDTDIR=../../lib
-exec java -cp build:$JDTDIR/org.eclipse.core.contenttype.jar:$JDTDIR/org.eclipse.core.jobs.jar:$JDTDIR/org.eclipse.core.resources.jar:$JDTDIR/org.eclipse.core.runtime.jar:$JDTDIR/org.eclipse.equinox.common.jar:$JDTDIR/org.eclipse.equinox.preferences.jar:$JDTDIR/org.eclipse.jdt.core.jar:$JDTDIR/org.eclipse.osgi.jar Java2Xml "$@"
