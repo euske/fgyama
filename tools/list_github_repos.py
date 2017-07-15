964b78a2aa6bcb288846e8502cd916ef85128d53
Yusuke Shinyama <yusuke@shinyama.jp>
2017-07-15 14:32:20 +0900

    Fixed: rate limiting.

--- tools/list_github_repos.py
+++ tools/list_github_repos.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python
+import os.path
 import sys
 import json
-from urllib.request import urlopen
+import time
+from urllib.request import URLopener
 from urllib.parse import urljoin
 
 URLBASE = 'https://api.github.com/'
+with open(os.path.expanduser('~/.github_token')) as fp:
+    TOKEN = fp.read().strip()
 
 def get_search_url(language, minstars=100, perpage=100, page=1):
     return urljoin(
@@ -12,18 +16,21 @@ def get_search_url(language, minstars=100, perpage=100, page=1):
         '/search/repositories?q=language:%s+stars:>%s&page=%s&per_page=%s' %
         (language, minstars, page+1, perpage))
 
-def get_repo_url(full_name):
+def get_repo_url(full_name, branch_name):
     return urljoin(
         URLBASE,
-        '/repos/%s/branches' % full_name)
+        '/repos/%s/branches/%s' % (full_name, branch_name))
 
-def call_api(url):
-    fp = urlopen(url)
+def call_api(url, wait=1):
+    time.sleep(wait)
+    req = URLopener()
+    req.addheader('Authorization', 'token '+TOKEN)
+    fp = req.open(url)
     data = json.load(fp)
     fp.close()
     return data
 
-def main(argv):
+def list_repos():
     npages = 10
     for page in range(npages):
         data = call_api(get_search_url('java', page=page))
@@ -31,6 +38,23 @@ def main(argv):
             full_name = item['full_name']
             default_branch = item['default_branch']
             print (full_name, default_branch)
+        sys.stdout.flush()
+    return
+
+def list_commits():
+    import fileinput
+    for line in fileinput.input():
+        (full_name, default_branch) = line.strip().split(' ')
+        repo = call_api(get_repo_url(full_name, default_branch))
+        commit = repo['commit']
+        sha = commit['sha']
+        print (full_name, default_branch, sha)
+        sys.stdout.flush()
+    return 0
+
+def main(argv):
+    #list_commits()
+    list_repos()
     return 0
 
 if __name__ == '__main__': sys.exit(main(sys.argv))
