83eeaed7a7877a6a777c42f1ff38206629edd8b9
Yusuke Shinyama <yusuke@shinyama.jp>
2017-10-09 17:39:49 +0900

    Added: html output.

--- tools/finditer.py
+++ tools/finditer.py
@@ -3,6 +3,55 @@ import sys
 from graph import SourceDB, DFGraph
 from graph import get_graphs
 
+def q(s):
+    return s.replace('&','&amp;').replace('>','&gt;').replace('<','&lt;').replace('"','&quot;')
+
+def show_html_headers():
+    print('''<html>
+<style>
+pre { margin: 1em; border: 1px solid gray;}
+.pair { border: 2px solid black; margin: 1em; }
+.s0 { background: #ddffff; }
+.s1 { background: #ffffdd; }
+.src { font-size: 50%; font-weight: bold; margin: 1em; }
+.p0 { background:#ffff00; color:black; }
+.p1 { background:#00ffff; color:black; }
+.p2 { background:#88ff88; color:black; }
+.p3 { background:#ff88ff; color:black; }
+.p4 { background:#8888ff; color:black; }
+.p5 { background:#ff0000; color:white; }
+.p6 { background:#008800; color:white; }
+.p7 { background:#0000ff; color:white; }
+.p8 { background:#004488; color:white; }
+.p9 { background:#884400; color:white; }
+</style>
+<body>
+''')        
+    return
+
+def show_html(src, nodes):
+    d = {}
+    for (i,n) in enumerate(nodes):
+        d[n.nid] = i
+    def astart(nid):
+        return '<span class="p%s">' % d[nid]
+    def aend(anno):
+        return '</span>'
+    def abody(annos, s):
+        return q(s.replace('\n',''))
+    def println(lineno, s):
+        if lineno is None:
+            print ('     '+s)
+        else:
+            lineno += 1
+            print ('%5d:%s' % (lineno, s))
+        return
+    print('<div class=src>%s: </div>' % (src.name))
+    print('<pre>')
+    src.show_nodes(nodes, println=println, astart=astart, aend=aend, abody=abody)
+    print('</pre>')
+    return
+
 def isiter(ref, n_begin, n_end):
     #print (ref, n_begin, n_end)
     def isref(n):
@@ -41,19 +90,23 @@ def finditer(graph):
 def main(argv):
     import getopt
     def usage():
-        print('usage: %s [-B basedir] [graph ...]' % argv[0])
+        print('usage: %s [-B basedir] [-H] [graph ...]' % argv[0])
         return 100
     try:
-        (opts, args) = getopt.getopt(argv[1:], 'B:')
+        (opts, args) = getopt.getopt(argv[1:], 'vB:H')
     except getopt.GetoptError:
         return usage()
     verbose = False
     srcdb = None
+    html = False
     for (k, v) in opts:
         if k == '-v': verbose = True
         elif k == '-B': srcdb = SourceDB(v)
+        elif k == '-H': html = True
     if not args: return usage()
-    
+
+    if html:
+        show_html_headers()
     for graph in get_graphs(args.pop(0)):
         src = None
         if srcdb is not None:
@@ -62,10 +115,13 @@ def main(argv):
             except KeyError:
                 pass
         for ref in finditer(graph):
-            print (src, graph, ref)
             if src is not None:
                 nodes = [ node for node in graph.nodes.values() if node.ref == ref ]
-                src.show_nodes(nodes)
+                if html:
+                    show_html(src, nodes)
+                else:
+                    print (src, graph, ref)
+                    src.show_nodes(nodes)
                 print()
     return 0
 
