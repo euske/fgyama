c03c3bd0a393def37a2ebe6799ab4b1ce3245e7c
Yusuke Shinyama <yusuke@shinyama.jp>
2017-11-11 11:51:13 +0900

    Added: srcmap.db to zipflatten.py

--- tools/zipflatten.py
+++ tools/zipflatten.py
@@ -7,23 +7,50 @@ import sys
 import re
 import os.path
 import zipfile
+import sqlite3
 
 BUFSIZ = 2**16
+
+def build_srcmap_tables(cur):
+    cur.executescript('''
+CREATE TABLE SourcePath (
+    Uid INTEGER PRIMARY KEY,
+    FileName TEXT,
+    ZipFile TEXT,
+    SrcPath TEXT
+);
+CREATE INDEX SourcePathIndex ON SourcePath(FileName);
+''')
+    return
 
 def main(argv):
     import getopt
     def usage():
-        print('usage: %s [-p pat] [zip ...]' % argv[0])
+        print('usage: %s [-n] [-p pat] [-M srcmap.db] [zip ...]' % argv[0])
         return 100
     try:
-        (opts, args) = getopt.getopt(argv[1:], 'p:')
+        (opts, args) = getopt.getopt(argv[1:], 'np:M:')
     except getopt.GetoptError:
         return usage()
     debug = 0
+    extract = True
     pat = None
+    srcmap = None
     for (k, v) in opts:
-        if k == '-p': pat = re.compile(v)
+        if k == '-n': extract = False
+        elif k == '-p': pat = re.compile(v)
+        elif k == '-M': srcmap = v
     if not args: return usage()
+    
+    srcmapconn = srcmapcur = None
+    if srcmap is not None:
+        srcmapconn = sqlite3.connect(srcmap)
+        srcmapcur = srcmapconn.cursor()
+        try:
+            build_srcmap_tables(srcmapcur)
+        except sqlite3.OperationalError:
+            pass
+    
     for zippath in args:
         zfp = zipfile.ZipFile(zippath)
         outdir = None
@@ -35,20 +62,28 @@ def main(argv):
             (dir1,_,dst) = src.partition('/')
             if outdir != dir1:
                 outdir = dir1
-                try:
-                    os.makedirs(outdir)
-                except OSError:
-                    pass
+                if extract:
+                    try:
+                        os.makedirs(outdir)
+                    except OSError:
+                        pass
             dst = os.path.join(outdir, dst.replace('/','_'))
             print('extract: %r -> %r' % (src, dst), file=sys.stderr)
-            fp = zfp.open(src, 'r')
-            out = open(dst, 'wb')
-            while 1:
-                data = fp.read(BUFSIZ)
-                if not data: break
-                out.write(data)
-            out.close()
-            fp.close()
+            if srcmapcur is not None:
+                srcmapcur.execute('INSERT INTO SourcePath VALUES (NULL,?,?,?);',
+                                  (dst, os.path.basename(zippath), src))
+            if extract:
+                fp = zfp.open(src, 'r')
+                out = open(dst, 'wb')
+                while 1:
+                    data = fp.read(BUFSIZ)
+                    if not data: break
+                    out.write(data)
+                out.close()
+                fp.close()
+    
+    if srcmapconn is not None:
+        srcmapconn.commit()
     return 0
 
 if __name__ == '__main__': sys.exit(main(sys.argv))
