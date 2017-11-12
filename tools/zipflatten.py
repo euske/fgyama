e74d9a552a68b0bafe9fb5669bdaf3330a844434
Yusuke Shinyama <yusuke@shinyama.jp>
2017-11-12 19:36:20 +0900

    Added: comprehensive source map preservation at zipflatten.py.

--- tools/zipflatten.py
+++ tools/zipflatten.py
@@ -7,40 +7,56 @@ import sys
 import re
 import os.path
 import zipfile
+import hashlib
 import sqlite3
 
 BUFSIZ = 2**16
 
 def build_srcmap_tables(cur):
     cur.executescript('''
-CREATE TABLE SourcePath (
+CREATE TABLE SourceMap (
     Uid INTEGER PRIMARY KEY,
     FileName TEXT,
-    ZipFile TEXT,
+    RepoName TEXT,
+    BranchName TEXT,
+    CommitId TEXT,
     SrcPath TEXT
 );
-CREATE INDEX SourcePathIndex ON SourcePath(FileName);
+CREATE INDEX SourceMapIndex ON SourceMap(FileName);
 ''')
     return
+
+NONASCII = re.compile(r'[^.a-zA-Z0-9]')
+def getkey(path):
+    h = hashlib.md5(path.encode('utf-8')).hexdigest()
+    name = os.path.basename(path)
+    name = NONASCII.sub(lambda m:'_%04x' % ord(m.group(0)), name)
+    return h+'_'+name
 
 def main(argv):
     import getopt
     def usage():
-        print('usage: %s [-n] [-p pat] [-M srcmap.db] [zip ...]' % argv[0])
+        print('usage: %s [-n] [-p pat] [-b dstbase] [-R repos.out] [-M srcmap.db] [zip ...]' %
+              argv[0])
         return 100
     try:
-        (opts, args) = getopt.getopt(argv[1:], 'np:M:')
+        (opts, args) = getopt.getopt(argv[1:], 'np:b:R:M:')
     except getopt.GetoptError:
         return usage()
     debug = 0
     extract = True
     pat = None
+    dstbase = '.'
+    repomap = None
     srcmap = None
     for (k, v) in opts:
         if k == '-n': extract = False
         elif k == '-p': pat = re.compile(v)
+        elif k == '-b': dstbase = v
+        elif k == '-R': repomap = v
         elif k == '-M': srcmap = v
     if not args: return usage()
+    assert (srcmap is None) == (repomap is None)
     
     srcmapconn = srcmapcur = None
     if srcmap is not None:
@@ -50,31 +66,43 @@ def main(argv):
             build_srcmap_tables(srcmapcur)
         except sqlite3.OperationalError:
             pass
+
+    repo = None
+    if repomap is not None:
+        repo = {}
+        with open(repomap) as fp:
+            for line in fp:
+                (reponame,branch,commit) = line.strip().split(' ')
+                repo[commit] = (reponame, branch)
     
     for zippath in args:
         zfp = zipfile.ZipFile(zippath)
-        outdir = None
+        dstdir = None
+        (commit,_) = os.path.splitext(os.path.basename(zippath))
+        if repo is not None:
+            (reponame,branch) = repo[commit]
         for info in zfp.infolist():
             if info.is_dir(): continue
             src = info.filename
             if '/.' in src: continue
             if pat is not None and not pat.search(src): continue
-            (dir1,_,dst) = src.partition('/')
-            if outdir != dir1:
-                outdir = dir1
+            (dir1,_,src1) = src.partition('/')
+            if dstdir != dir1:
+                dstdir = dir1
                 if extract:
                     try:
-                        os.makedirs(outdir)
+                        os.makedirs(os.path.join(dstbase, dstdir))
                     except OSError:
                         pass
-            dst = os.path.join(outdir, dst.replace('/','_'))
-            print('extract: %r -> %r' % (src, dst), file=sys.stderr)
+            dst = os.path.join(dstdir, getkey(src1))
+            print('extract: %r -> %r' % (src1, dst), file=sys.stderr)
             if srcmapcur is not None:
-                srcmapcur.execute('INSERT INTO SourcePath VALUES (NULL,?,?,?);',
-                                  (dst, os.path.basename(zippath), src))
+                srcmapcur.execute(
+                    'INSERT INTO SourceMap VALUES (NULL,?,?,?,?,?);',
+                    (dst, reponame, branch, commit, src1))
             if extract:
                 fp = zfp.open(src, 'r')
-                out = open(dst, 'wb')
+                out = open(os.path.join(dstbase, dst), 'wb')
                 while 1:
                     data = fp.read(BUFSIZ)
                     if not data: break
