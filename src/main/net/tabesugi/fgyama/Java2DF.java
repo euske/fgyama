d9006bcb8cbe7d7bf8d650314965879ca62333a6
Yusuke Shinyama <yusuke@shinyama.jp>
2018-03-06 18:26:56 +0900

    Separated: PackageNameExtractor

--- src/main/net/tabesugi/fgyama/Java2DF.java
+++ src/main/net/tabesugi/fgyama/Java2DF.java
@@ -643,69 +643,14 @@ class NameResolutionChecker extends ASTVisitor {
 	}
 	return false;
     }
-}
 
-
-//  PackageNameExtractor
-//
-class PackageNameExtractor extends ASTVisitor {
-
-    private String _package = null;
-    private String _toplevel = null;
-
-    public String getPackageName() {
-	return _package;
-    }
-
-    public String getTopLevelName() {
-	return _toplevel;
-    }
-
-    public boolean visit(PackageDeclaration node) {
-	_package = node.getName().getFullyQualifiedName();
-	return false;
-    }
-
-    public boolean visit(TypeDeclaration node) {
-	if (node.isPackageMemberTypeDeclaration()) {
-	    _toplevel = node.getName().getIdentifier();
-	}
-	return false;
-    }
-
-    public boolean visit(EnumDeclaration node) {
-	if (node.isPackageMemberTypeDeclaration()) {
-	    _toplevel = node.getName().getIdentifier();
-	}
-	return false;
-    }
-
-    public static String getCanonicalName(String path)
-	throws IOException {
-	String src = Utils.readFile(path);
-	Map<String, String> options = JavaCore.getOptions();
-	JavaCore.setComplianceOptions(JavaCore.VERSION_1_7, options);
-	ASTParser parser = ASTParser.newParser(AST.JLS8);
-        parser.setUnitName(path);
-	parser.setSource(src.toCharArray());
-	parser.setKind(ASTParser.K_COMPILATION_UNIT);
-	parser.setResolveBindings(false);
-	parser.setEnvironment(null, null, null, false);
-	parser.setCompilerOptions(options);
-	PackageNameExtractor extractor = new PackageNameExtractor();
-	CompilationUnit cu = (CompilationUnit)parser.createAST(null);
-	cu.accept(extractor);
-	String pkg = extractor.getPackageName();
-	String name = extractor.getTopLevelName();
-        if (name == null) {
-            return null;
-        } else if (pkg == null) {
-            return name;
-        } else {
-            return pkg+"."+name;
-        }
+    public static boolean canResolveNames(ASTNode node) {
+	NameResolutionChecker checker = new NameResolutionChecker();
+	node.accept(checker);
+	return checker.isResolved();
     }
 }
+
 
 //  Java2DF
 //
@@ -2067,9 +2012,7 @@ public class Java2DF extends ASTVisitor {
 	if (method.getBody() == null) return false;
 	String funcName = method.getName().getFullyQualifiedName();
 	if (this.resolve) {
-	    NameResolutionChecker checker = new NameResolutionChecker();
-	    method.accept(checker);
-	    if (!checker.isResolved()) {
+	    if (!NameResolutionChecker.canResolveNames(method)) {
 		Utils.logit("Unresolved: "+funcName);
 		return false;
 	    }
