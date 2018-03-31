c54b6fc377658c423f37dcbf8b8b78be371a3bc6
Yusuke Shinyama <yusuke@shinyama.jp>
2018-04-01 01:17:56 +1000

    Added: UnsupportedSyntax.java

---
+++ src/main/net/tabesugi/fgyama/UnsupportedSyntax.java
@@ -0,0 +1,21 @@
+//  Java2DF
+//
+package net.tabesugi.fgyama;
+import java.io.*;
+import java.util.*;
+import org.eclipse.jdt.core.*;
+import org.eclipse.jdt.core.dom.*;
+
+
+//  UnsupportedSyntax
+//
+public class UnsupportedSyntax extends Exception {
+
+    static final long serialVersionUID = 1L;
+
+    public ASTNode ast;
+
+    public UnsupportedSyntax(ASTNode ast) {
+	this.ast = ast;
+    }
+}
