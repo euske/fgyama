cf54868a166074d81d6856f148281e9b93f89231
Yusuke Shinyama <yusuke@shinyama.jp>
2019-02-21 07:44:44 +1000

    Added: annotator.

---
+++ annotator/helper.ts
@@ -0,0 +1,152 @@
+// helper.ts
+
+const CATEGORIES = [
+    'a:Interchangeable',
+    'b:Complementary',
+    'c:Inherited',
+    'd:Inclusive',
+    'e:Opposite',
+    'f:Same Signature',
+    'g:No Relation',
+    'u:Unknown'
+];
+
+var textarea: HTMLTextAreaElement = null;
+
+class Item {
+
+    category: string = '';
+    updated: number = 0;
+
+    load(cols: string[]) {
+        this.category = cols[2];
+        this.updated = parseInt(cols[4]);
+    }
+
+    save() {
+        return (this.category+','+this.updated);
+    }
+}
+
+interface ItemList {
+    [index: string]: Item;
+}
+
+function importText(data: ItemList, text: string) {
+    for (let line of text.split(/\n/)) {
+        line = line.trim();
+        if (line.length == 0) continue;
+        if (line.substr(0,1) == '#') continue;
+        let cols = line.split(/,/);
+        if (2 <= cols.length) {
+            let cid = cols.shift();
+            let item = new Item();
+            item.load(cols);
+            data[cid] = item;
+        }
+    }
+}
+
+function exportText(fieldName: string, data: ItemList): string {
+    let cids = Object.getOwnPropertyNames(data);
+    let lines = [] as string[];
+    lines.push('#START');
+    for (let cid of cids.sort()) {
+        let item = data[cid];
+        lines.push(fieldName+':'+cid+','+item.save());
+    }
+    lines.push('#END');
+    return lines.join('\n');
+}
+
+function toArray(coll: HTMLCollectionOf<Element>): Element[] {
+    let a = [] as Element[];
+    for (let i = 0; i < coll.length; i++) {
+        a.push(coll[i]);
+    }
+    return a;
+}
+
+function updateHTML(data: ItemList) {
+    let cids = Object.getOwnPropertyNames(data);
+    for (let cid of cids) {
+        let item = data[cid];
+        let sel2 = document.getElementById('SC'+cid) as HTMLSelectElement;
+        if (sel2 !== null) {
+            sel2.value = item.category;
+        }
+    }
+}
+
+function saveText(fieldName: string) {
+    if (window.localStorage) {
+        window.localStorage.setItem(fieldName, textarea.value);
+    }
+}
+
+function initData(fieldName: string): ItemList {
+    let data: ItemList = {};
+
+    function onCategoryChanged(ev: Event) {
+        let e = ev.target as HTMLSelectElement;
+        let cid = e.id.substr(2);
+        let item = data[cid];
+        item.category = e.value;
+        item.updated = Date.now();
+        textarea.value = exportText(fieldName, data);
+	saveText(fieldName);
+    }
+
+    function makeSelect(e: Element, id: string, choices: string[]) {
+        let select = document.createElement('select');
+        select.id = id;
+        for (let k of choices) {
+            let i = k.indexOf(':');
+            let option = document.createElement('option');
+            let v = k.substr(0, i);
+            option.setAttribute('value', v);
+            option.innerText = v+')'+k.substr(i+1);
+            select.appendChild(option);
+        }
+        e.appendChild(select);
+        return select;
+    }
+
+    function makeCheckbox(e: Element, id: string, caption: string) {
+        let label = document.createElement('label');
+        let checkbox = document.createElement('input');
+        checkbox.id = id;
+        checkbox.setAttribute('type', 'checkbox');
+        label.appendChild(checkbox);
+        label.appendChild(document.createTextNode(' '+caption));
+        e.appendChild(label);
+        return checkbox;
+    }
+
+    for (let e of toArray(document.getElementsByClassName('ui'))) {
+        let cid = e.id;
+        let sel2 = makeSelect(e, 'SC'+cid, CATEGORIES);
+        sel2.addEventListener('change', onCategoryChanged);
+        e.appendChild(document.createTextNode(' '));
+        data[cid] = new Item();
+    }
+    return data;
+}
+
+var curdata: ItemList = null;
+function run(fieldName: string, id: string) {
+    function onTextChanged(ev: Event) {
+        importText(curdata, textarea.value);
+        updateHTML(curdata);
+	saveText(fieldName);
+    }
+    textarea = document.getElementById(id) as HTMLTextAreaElement;
+    if (window.localStorage &&
+        window.localStorage.getItem(fieldName)) {
+        textarea.value = window.localStorage.getItem(fieldName);
+    }
+    textarea.addEventListener('input', onTextChanged);
+    curdata = initData(fieldName);
+    importText(curdata, textarea.value);
+    updateHTML(curdata);
+}
---
+++ annotator/srcdb.py
@@ -0,0 +1,183 @@
+#!/usr/bin/env python
+import sys
+import os.path
+import sqlite3
+
+
+##  SourceMap
+##
+class SourceMap:
+
+    def __init__(self, path):
+        self._conn = sqlite3.connect(path)
+        self._cur = self._conn.cursor()
+        try:
+            self._cur.executescript('''
+CREATE TABLE SourceMap (
+    Uid INTEGER PRIMARY KEY,
+    FileName TEXT,
+    RepoName TEXT,
+    BranchName TEXT,
+    CommitId TEXT,
+    SrcPath TEXT
+);
+CREATE INDEX SourceMapIndex ON SourceMap(FileName);
+''')
+        except sqlite3.OperationalError:
+            pass
+        return
+
+    def close(self):
+        self._conn.commit()
+        return
+
+    def add(self, path, reponame, branch, commit, src):
+        self._cur.execute(
+            'INSERT INTO SourceMap VALUES (NULL,?,?,?,?,?);',
+            (path, reponame, branch, commit, src))
+        return
+
+    def get(self, key):
+        rows = self._cur.execute(
+            'SELECT RepoName,CommitId,SrcPath FROM SourceMap WHERE FileName=?;',
+            (key,))
+        if not rows: raise KeyError(key)
+        return rows.fetchone()
+
+    def geturl(self, key):
+        (reponame,commit,srcpath) = self.get(key)
+        url = 'https://github.com/%s/tree/%s/%s' % (reponame, commit, srcpath)
+        return url
+
+
+##  SourceFile
+##
+class SourceFile:
+
+    def __init__(self, name, data):
+        self.name = name
+        self.data = data
+        self.lines = data.splitlines(True)
+        return
+
+    def __repr__(self):
+        return ('<SourceFile(%s)>' %
+                (self.name,))
+
+    def show_nodes(self, nodes,
+                   astart=(lambda _: '['),
+                   aend=(lambda _: ']'),
+                   abody=(lambda _,s: s),
+                   ncontext=1, skip='...\n'):
+        ranges = []
+        for node in nodes:
+            if node.ast is None: continue
+            (_,i,n) = node.ast
+            ranges.append((i, i+n, node.nid))
+        return self.show(ranges, ncontext=ncontext, skip=skip,
+                         astart=astart, aend=aend, abody=abody)
+
+    # ranges=[(start,end,anno), ...]
+    def show(self, ranges,
+             astart=(lambda _: '['),
+             aend=(lambda _: ']'),
+             abody=(lambda _,s: s),
+             ncontext=1, skip='...\n'):
+        for (lineno,line) in self.chunk(ranges, ncontext=ncontext):
+            if lineno is None:
+                yield (lineno,skip)
+            else:
+                buf = ''
+                for (v, anno, s) in line:
+                    if v == 0:
+                        buf += abody(anno, s)
+                    elif v < 0:
+                        buf += astart(anno)
+                    else:
+                        buf += aend(anno)
+                yield (lineno,buf)
+        return
+
+    # ranges=[(start,end,anno), ...]
+    def chunk(self, ranges, ncontext=1):
+        if not ranges: return
+        triggers = []
+        for (s,e,anno) in ranges:
+            triggers.append((s,-1,anno))
+            triggers.append((e,+1,anno))
+        triggers.sort(key=lambda x: (x[0],x[1]))
+        lines = {}
+        loc0 = 0
+        i = 0
+        annos = []
+        for (lineno,line) in enumerate(self.lines):
+            loc1 = loc0+len(line)
+            pos0 = 0
+            out = []
+            while i < len(triggers):
+                (loc,v,anno) = triggers[i]
+                if loc1 < loc: break
+                i += 1
+                pos1 = loc - loc0
+                out.append((0, annos[:], line[pos0:pos1]))
+                pos0 = pos1
+                if v < 0:
+                    out.append((v, anno, None))
+                    annos.append(anno)
+                else:
+                    out.append((v, anno, None))
+                    annos.remove(anno)
+            if out:
+                out.append((0, annos[:], line[pos0:]))
+                lines[lineno] = out
+            elif annos:
+                lines[lineno] = [(0, annos[:], line)]
+            loc0 = loc1
+        n = len(self.lines)
+        for (lineno,line) in list(lines.items()):
+            for i in range(max(0, lineno-ncontext),
+                           min(n, lineno+ncontext+1)):
+                if i not in lines:
+                    lines[i] = [(0, [], self.lines[i])]
+        lineno0 = 0
+        for lineno1 in sorted(lines):
+            if lineno0 < lineno1:
+                yield (None, None)
+            yield (lineno1, lines[lineno1])
+            lineno0 = lineno1+1
+        if lineno0 < len(self.lines):
+            yield (None, None)
+        return
+
+
+##  SourceDB
+##
+class SourceDB:
+
+    def __init__(self, basedir, encoding=None):
+        self.basedir = basedir
+        self.encoding = encoding
+        self._cache = {}
+        return
+
+    def get(self, name):
+        if name in self._cache:
+            src = self._cache[name]
+        else:
+            path = os.path.join(self.basedir, name)
+            try:
+                with open(path, encoding=self.encoding) as fp:
+                    data = fp.read()
+            except IOError:
+                raise KeyError(name)
+            except UnicodeError:
+                raise KeyError(name)
+            src = SourceFile(name, data)
+            self._cache[name] = src
+        return src
+
+    def show(self, fp=sys.stdout):
+        for src in self._cache.values():
+            for (_,line) in src.show():
+                fp.write(line)
+        return
