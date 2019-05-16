76ac7b69df4725b81ae8e899a126c5aea19eb288
Yusuke Shinyama <yusuke@shinyama.jp>
2019-05-16 14:54:59 +0900

    Removed: DFLink.java (merged)

--- src/main/net/tabesugi/fgyama/DFNode.java
+++ src/main/net/tabesugi/fgyama/DFNode.java
@@ -8,6 +8,36 @@ import org.eclipse.jdt.core.dom.*;
 import org.w3c.dom.*;
 
 
+//  DFLink
+//
+class DFLink {
+
+    private DFNode _dst;
+    private DFNode _src;
+    private String _label;
+
+    public DFLink(DFNode dst, DFNode src, String label) {
+        _dst = dst;
+        _src = src;
+        _label = label;
+    }
+
+    @Override
+    public String toString() {
+        return ("<DFLink "+_dst+"<-"+_src+">");
+    }
+
+    public Element toXML(Document document) {
+        Element elem = document.createElement("link");
+        elem.setAttribute("src", _src.getNodeId());
+        if (_label != null) {
+            elem.setAttribute("label", _label);
+        }
+        return elem;
+    }
+}
+
+
 //  DFNode
 //
 public class DFNode implements Comparable<DFNode> {
