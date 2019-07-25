8347ad38ea67ad559c3a374a4caa7a8544d13f98
Yusuke Shinyama <yusuke@shinyama.jp>
2019-07-25 15:25:42 +0900

    Changed: nameconsistency.py

--- tools/nameconsistency.py
+++ tools/nameconsistency.py
@@ -1,89 +1,92 @@
 #!/usr/bin/env python
-#
-# check name consistency.
-#
 import sys
 import re
-from srcdb import SourceDB, SourceAnnot
 from naivebayes import NaiveBayes
 
-WORD1 = re.compile(r'[A-Z]?[a-z]+$')
-WORD2 = re.compile(r'[A-Z]+$')
-def getnoun(name):
-    if name[-1].islower():
-        return WORD1.search(name).group(0).lower()
-    elif name[-1].isupper():
-        return WORD2.search(name).group(0).lower()
+NAME = re.compile(r'\w+$', re.U)
+def stripid(name):
+    m = NAME.search(name)
+    if m:
+        return m.group(0)
     else:
         return None
 
-class Chain:
-    def __init__(self, ref0, ref1, feats, nodes):
-        self.ref0 = ref0
-        self.ref1 = ref1
-        self.noun0 = getnoun(ref0)
-        self.noun1 = getnoun(ref1)
-        self.feats = feats
-        self.nodes = nodes
-        return
+WORD = re.compile(r'[a-z]+[A-Z]?|[A-Z]+|[0-9]+')
+def splitwords(s):
+    """
+    >>> splitwords('name')
+    ['name']
+    >>> splitwords('this_is_name_!')
+    ['name', 'is', 'this']
+    >>> splitwords('thisIsName')
+    ['name', 'is', 'this']
+    >>> splitwords('SomeXMLStuff')
+    ['stuff', 'xml', 'some']
+"""
+    if s is None: return []
+    n = len(s)
+    r = ''.join(reversed(s))
+    return [ s[n-m.end(0):n-m.start(0)].lower() for m in WORD.finditer(r) ]
 
+# main
 def main(argv):
     import fileinput
+    import getopt
+    def usage():
+        print('usage: %s [-d] [graph ...]' % argv[0])
+        return 100
+    try:
+        (opts, args) = getopt.getopt(argv[1:], 'd')
+    except getopt.GetoptError:
+        return usage()
+    debug = 0
+    for (k, v) in opts:
+        if k == '-d': debug += 1
+    if not args: return usage()
 
-    srcdb = SourceDB('.')
-    nb0 = NaiveBayes()
-    nb1 = NaiveBayes()
-    chains = []
-    feats2c = {}
-    for line in fileinput.input():
-        line = line.strip()
-        if line.startswith('+SOURCE'):
-            (_,_,s) = line.partition(' ')
-            (fid,name) = eval(s)
-            srcdb.register(fid, name)
-        elif line.startswith('+NODES'):
-            f = line.split()[1:]
-            nodes = []
-            for k in f:
-                if k == 'None': continue
-                (fid,start,end) = k.split(':')
-                nodes.append((int(fid), int(start), int(end)))
-        elif line.startswith('+PAIR'):
-            f = line.split()[1:]
-            feats = tuple(f[2:])
-            c = Chain(f[0], f[1], feats, nodes)
-            chains.append(c)
-            if feats in feats2c:
-                a = feats2c[feats]
-            else:
-                a = feats2c[feats] = []
-            nb1.add(c.noun1, (c.noun0, feats))
-            nb0.add(c.noun0, (c.noun1, feats))
-            a.append(c)
-    nb0.commit()
-    nb1.commit()
-
-    T = 10
-    for c in chains:
-        cands1 = nb1.get((c.noun0, c.feats))
-        (n1,p1) = cands1[0]
-        if 2 < len(cands1):
-            (n2,p2) = cands1[1]
-            if p1 < p2*T: continue
-        if c.noun1 == n1: continue
-        print('#', c.noun0, c.noun1, '->', n1, c.feats)
-        annot = SourceAnnot(srcdb)
-        for (fid,start,end) in (c.nodes[0], c.nodes[-1]):
-            annot.addbyfid(fid, start, end)
-        annot.show_text()
-        for c2 in feats2c[c.feats]:
-            if c2.noun1 != n1: continue
-            print('!', c2.feats)
-            annot = SourceAnnot(srcdb)
-            for (fid,start,end) in (c2.nodes[0], c2.nodes[-1]):
-                annot.addbyfid(fid, start, end)
-            annot.show_text()
-        print()
+    path = args.pop(0)
+    print('Loading: %r...' % path, file=sys.stderr)
+    feats = {}
+    with open(path) as fp:
+        a = None
+        for line in fp:
+            if line.startswith('! '):
+                data = eval(line[2:])
+                if data[0] == 'REF':
+                    item = data[1]
+                    if item in feats:
+                        a = feats[item]
+                    else:
+                        a = feats[item] = {}
+                else:
+                    a = None
+            elif a is not None and line.startswith('+ '):
+                (n,_,line) = line[2:].partition(' ')
+                data = eval(line)
+                feat = data[0:4]
+                if feat not in a:
+                    a[feat] = 0
+                a[feat] += int(n)
+    #
+    nb = NaiveBayes()
+    for (item,a) in feats.items():
+        name = stripid(item)
+        words = splitwords(name)
+        for w in words:
+            nb.add(w, a.keys())
+    nb.commit()
+    #
+    nitems = 0
+    for (item,a) in feats.items():
+        name = stripid(item)
+        words = splitwords(name)
+        cands = nb.get(a)
+        n = len(words)
+        if sorted(words) != sorted(cands[:n]):
+            print(item)
+            print('#', [ (w,p) for (w,p,_) in cands[:n+1] ])
+        nitems += 1
+    print(nitems)
 
     return 0
 
