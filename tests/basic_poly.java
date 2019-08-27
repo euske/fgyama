f622cb1e13dc1b6941161fa25e2b6db5b804456f
Yusuke Shinyama <yusuke@shinyama.jp>
2019-08-27 19:37:26 +0900

    Updated: test cases.

--- tests/basic_poly.java
+++ tests/basic_poly.java
@@ -7,18 +7,20 @@ class basic_poly_Foo {
 
 class basic_poly_Baa extends basic_poly_Foo {
     public int moo(basic_poly_Foo obj) {
-        super.moo(obj);
+        super.moo(obj);		// Foo.moo is called.
         return obj.x+1;
     }
+}
 
-    public static void main() {
+public class basic_poly {
+    public static void main(String[] args) {
         basic_poly_Foo a = new basic_poly_Foo();
-        int x = a.moo(a);
+        int x = a.moo(a);	// Foo.moo is called.
         basic_poly_Foo b = new basic_poly_Baa();
-        x = b.moo(a);
-        x = b.moo(b);
+        x = b.moo(a);		// Baa.moo is called.
+        x = b.moo(b);		// Baa.moo is called.
         basic_poly_Baa c = new basic_poly_Baa();
-        x = c.moo(a);
-        x = c.moo(b);
+        x = c.moo(a);		// Baa.moo is called.
+        x = c.moo(b);		// Baa.moo is called.
     }
 }
