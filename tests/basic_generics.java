a23813562fddec80ab3873177d78206b398e24be
Yusuke Shinyama <yusuke@shinyama.jp>
2020-01-09 13:49:41 +0900

    Updated: tests/basic_generics.java

--- tests/basic_generics.java
+++ tests/basic_generics.java
@@ -13,6 +13,10 @@ public class basic_generics {
             return y;
         }
 
+        <S> S get2(Moo<S> x) {
+            return x.get();
+        }
+
         Moo<T> copy() {
             return new Moo<T>(x);
         }
@@ -23,6 +27,7 @@ public class basic_generics {
     public static void test() {
         Moo<String> moo = new Moo<String>("moo");
         String b = moo.get().toLowerCase();
+        String c = moo.get2(moo).toLowerCase();
         Moo<String> boo = moo.copy();
         Moo<Integer>.Baa mi;
     }
