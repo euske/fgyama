f602f13d4f1bb134c321ecf3b0cba55b7c0a878e
Yusuke Shinyama <yusuke@shinyama.jp>
2020-10-27 16:02:37 +0900

    Tools cleanup.

---
+++ tools/README.md
@@ -0,0 +1,38 @@
+# fgyama.tools
+
+## General tools
+
+- `java2df.sh`: Java2DF command line wrapper.
+- `graph.py`: Base library for parsing/manipulating graphs.
+  (defines: DFNode, DFKlass, DFMethod, GraphDB)
+- `interproc.py`: Handle interprocedural graphs.
+- `getwords.py`: Identifier manipulation.
+- `graph2db.py`: Creates graph indices.
+- `graph2gv.py`: Convert a graph into GraphViz.
+- `viewgraph.py`: Command line graph viewer.
+- `naivebayes.py`: Naive Bayes framework.
+- `vsm.py`: VSM framework.
+- `srcdb.py`: SourceDB.
+
+- `list_github_repos.py`: List GitHub repos.
+- `zipflatten.py`: Flatten a zip file.
+- `dumpxml.py`: Pretty print XML.
+
+## Research related tools
+
+- `listmethods.py`:
+- `listvars.py`:
+- `graph2feats.py`:
+- `featdb.py`: FeatDB.
+- `viewfeats.py`:
+- `namecon.py`:
+- `cmpnamecon.py`:
+- `groupvars.py`:
+- `simname.py`:
+- `simtoken.py`:
+- `simvars.py`:
+- `summarizedf.py`:
+- `synhom.py`:
+- `viewnamecon.py`:
+- `viewsimvars.py`:
+- `wordrel.py`:
--- tools/obsolete/cmpfeats.py
+++ tools/obsolete/cmpfeats.py
@@ -1,63 +1,47 @@
 #!/usr/bin/env python
 import sys
+import os
 from featdb import FeatDB
-from getwords import stripid, splitwords
-from vsm import VSM
 
-def get(d, k, v):
-    if k in d:
-        a = d[k]
-    else:
-        a = d[k] = v
-    return a
-
+# main
 def main(argv):
+    import fileinput
     import getopt
     def usage():
-        print('usage: %s [-t threshold] featdb1 featdb2 ...' % argv[0])
+        print(f'usage: {argv[0]} [-d] feats0.db feats0_a.db ... - feats1.db feats1_a.db ...')
         return 100
     try:
-        (opts, args) = getopt.getopt(argv[1:], 't:')
+        (opts, args) = getopt.getopt(argv[1:], 'd')
     except getopt.GetoptError:
         return usage()
-
-    threshold = 0.5
+    debug = 0
     for (k, v) in opts:
-        if k == '-t': threshold = float(v)
+        if k == '-d': debug += 1
+    if not args: return usage()
 
-    relword = {}
-    ws = []
-    for (i,path) in enumerate(args):
-        print('Loading: %d: %r...' % (i,path), file=sys.stderr)
-        db = FeatDB(path)
-        wordfeats = {}
-        for (tid,item) in db.get_items():
-            feats = db.get_feats(tid, resolve=True)
-            name = stripid(item)
-            words = splitwords(name)
-            for (i,w0) in enumerate(words):
-                for w1 in words[i+1:]:
-                    get(relword, w0, []).append(w1)
-                    get(relword, w1, []).append(w0)
-            for w in words:
-                fs = get(wordfeats, w, {})
-                for f in feats.keys():
-                    if f not in fs:
-                        fs[f] = 0
-                    fs[f] += 1
-        ws.append(wordfeats)
-
-    sp = VSM()
-    for (i,wordfeats) in enumerate(ws):
-        for (w,fs) in wordfeats.items():
-            sp.add((i,w), fs)
-    sp.commit()
-
-    for (sim,(i0,w0),(i1,w1)) in sp.findall(threshold=threshold, verbose=True):
-        if w0 == w1: continue
-        if w1 in relword and w0 in relword[w1]: continue
-        if w0 in relword and w1 in relword[w0]: continue
-        print(sim, (i0,w0), (i1,w1))
+    db0 = items0 = None
+    for dbpath in args:
+        if dbpath == '-':
+            db0 = items0 = None
+            continue
+        db1 = FeatDB(dbpath)
+        items1 = sorted(db1.get_items())
+        if db0 is None:
+            db0 = db1
+            items0 = items1
+            print(f'src: {dbpath}, {len(items0)} items')
+            continue
+        #assert items0 == items1, (len(items0), len(items1))
+        items1 = dict(items1)
+        nfeats0 = nfeats1 = ncommon = 0
+        for (tid,item) in items0:
+            if tid not in items1: continue
+            feats0 = set(db0.get_feats(tid, resolve=True))
+            feats1 = set(db1.get_feats(tid, resolve=True))
+            nfeats0 += len(feats0)
+            nfeats1 += len(feats1)
+            ncommon += len(feats0.intersection(feats1))
+        print(f'  {dbpath}: {ncommon} / ({nfeats1}, {nfeats0})')
 
     return 0
 
