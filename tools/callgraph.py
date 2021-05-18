306aafce6b53ad5a0a557871053d93747ef8fcb7
Yusuke Shinyama <yusuke@shinyama.jp>
2021-05-18 14:40:46 +0900

    Code cleanup.

--- tools/callgraph.py
+++ tools/callgraph.py
@@ -28,12 +28,37 @@ def topsort(nodes):
             incoming[n1].remove(n0)
     return out
 
+class Group:
+
+    gid = 0
+
+    def __init__(self, method):
+        Group.gid += 1
+        self.gid = Group.gid
+        self.method = method
+        self.children = []
+        self.vin = None
+        self.vout = None
+        return
+
+    def add(self, group):
+        self.children.append(group)
+        return
+
 class Vertex:
+
     vid = 0
-    @classmethod
-    def newvid(klass):
-        klass.vid += 1
-        return klass.vid
+
+    def __init__(self, group):
+        Vertex.vid += 1
+        self.vid = Vertex.vid
+        self.group = group
+        self.linkto = []
+        return
+
+    def connect(self, vtx):
+        self.linkto.append(vtx)
+        return
 
 # main
 def main(argv):
@@ -74,17 +99,13 @@ def main(argv):
             methods.append(method)
             gid2method[method.name] = method
 
-    outedges = []
     def trace(method, prevs, cc=None):
         logging.info(f'trace {method}')
-        h = '  '*clen(cc)
-        (klass,name,func) = parsemethodname(method.name)
-        vout = Vertex.newvid()
-        out.write(h+f'subgraph {q("cluster_"+str(vout))} {{\n')
-        out.write(h+f'  label={q(stripid(klass.name)+"."+name)};\n')
-        out.write(h+f'  V{vout} [label={q("exit "+name)}];\n')
+        group = Group(method)
+        vout = Vertex(group)
+        group.vout = vout
         for vtx in prevs:
-            outedges.append((vout, vtx))
+            vout.connect(vtx)
         edges = { 'in': set() }
         for n0 in topsort(method):
             if n0 in edges:
@@ -99,6 +120,7 @@ def main(argv):
                     callee = gid2method[gid]
                     if cc is not None and callee in cc: continue
                     vtx = trace(callee, p, Cons(method, cc))
+                    group.add(vtx.group)
                     a.append(vtx)
                 if a:
                     p = set(a)
@@ -111,24 +133,46 @@ def main(argv):
                         edges[n1] = p.copy()
             else:
                 edges['in'].update(p)
-        vin = Vertex.newvid()
-        out.write(h+f'  V{vin} [label={q("enter "+name)}];\n')
-        out.write(h+f'}}\n')
+        vin = Vertex(group)
+        group.vin = vin
         for vtx in edges['in']:
-            outedges.append((vin, vtx))
+            vin.connect(vtx)
         return vin
 
-    out.write(f'digraph {q(path)} {{\n')
+    groups = []
     for method in methods:
         # Filter "top-level" methods only which aren't called by anyone else.
         if method.callers: continue
         (klass,name,func) = parsemethodname(method.name)
-        if (name not in targets) and (method.name not in targets): continue
-        trace(method, [])
-        break
+        #if (name not in targets) and (method.name not in targets): continue
+        vtx = trace(method, [])
+        groups.append(vtx.group)
+        #break
+
+    outedges = []
+    out.write(f'digraph {q(path)} {{\n')
+    def f(group, level=1):
+        h = '  '*level
+        (klass,name,func) = parsemethodname(group.method.name)
+        out.write(h+f'subgraph {q("cluster_"+str(group.gid))} {{\n')
+        out.write(h+f'  label={q(stripid(klass.name)+"."+name)};\n')
+        for g in group.children:
+            f(g, level+1)
+        vin = group.vin
+        out.write(h+f'  V{vin.vid} [label={q("enter "+name)}];\n')
+        for vtx in vin.linkto:
+            outedges.append((vin,vtx))
+        vout = group.vout
+        out.write(h+f'  V{vout.vid} [label={q("exit "+name)}];\n')
+        for vtx in vout.linkto:
+            outedges.append((vout,vtx))
+        out.write(h+'}\n')
+        return
+    for group in groups:
+        f(group)
     for (v0,v1) in outedges:
-        out.write(f'V{v0} -> V{v1};\n')
-    out.write(f'}}\n')
+        out.write(f'  V{v0.vid} -> V{v1.vid};\n')
+    out.write('}\n')
 
     return 0
 
