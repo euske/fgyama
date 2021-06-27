3caeb11f5fc6c02fbaede332a516e88a186b47e3
Yusuke Shinyama <yusuke@shinyama.jp>
2021-06-27 12:00:12 +0900

    Code cleanup.

--- tools/minecalls.py
+++ tools/minecalls.py
@@ -9,6 +9,57 @@ def gettail(name):
     (_,_,name) = name.rpartition('/')
     return name.lower()
 
+KINDS = {
+    'value', 'valueset',
+    'op_assign', 'op_prefix', 'op_infix', 'op_postfix',
+    'op_typecast', 'op_typecheck',
+    'ref_var', 'ref_array', 'ref_field',
+    'call', 'new',
+}
+VALUES = {'value', 'valueset'}
+CALLS = {'call', 'new'}
+REFS = {'ref_var', 'ref_field', 'ref_array'}
+CONDS = {'join', 'begin', 'end', 'case', 'catchjoin'}
+def ignored(n):
+    return (len(n.inputs) == 1 and n.kind not in KINDS)
+
+def getfeats(label, n):
+    if n.kind not in KINDS: return
+    if label.startswith('@'):
+        label = '@'
+    if n.kind in VALUES:
+        (_,t) = DFType.parse(n.ntype)
+        yield f'{label}:{n.kind}:{t}'
+    elif n.kind in REFS:
+        yield f'{label}:{n.kind}:{parserefname(n.ref)}'
+    elif n.kind in CALLS:
+        methods = n.data.split()
+        (_,name,_) = parsemethodname(methods[0])
+        yield f'{label}:{n.kind}:{name}'
+    else:
+        yield f'{label}:{n.kind}:{n.data}'
+    return
+
+def visit(out, label0, n0, visited, n=0):
+    global maxpathlen
+    if n0 in visited: return
+    visited.add(n0)
+    if ignored(n0):
+        for (label1,n1) in n0.inputs.items():
+            if label1 == '#bypass': continue
+            if label1.startswith('_'): continue
+            visit(out, label0, n1, visited, n)
+    else:
+        for feat in getfeats(label0, n0):
+            out.add((n, feat))
+        n += 1
+        if n < maxpathlen:
+            for (label1,n1) in n0.inputs.items():
+                if label1.startswith('_'): continue
+                if label1 == '#bypass': continue
+                if n1.kind == 'ref_array' and not label1: continue
+                visit(out, label1, n1, visited, n)
+
 funargs_path = {
     # new java.io.File(path)
     'Ljava/io/File;.<init>(Ljava/lang/String;)V': ['#arg0'],
@@ -392,6 +443,7 @@ add('height', funargs_height) # int
 
 # main
 def main(argv):
+    global maxpathlen
     import getopt
     def usage():
         print(f'usage: {argv[0]} [-d] [-M maxoverrides] [-m maxpathlen] graph.db')
@@ -411,71 +463,22 @@ def main(argv):
 
     logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', level=level)
 
-    path = args.pop(0)
-    logging.info(f'Loading: {path!r}...')
-    db = GraphDB(path)
+    for path in args:
+        logging.info(f'Loading: {path!r}...')
+        db = GraphDB(path)
+        #logging.info(f'Running...')
+        #builder.run()
 
-    #logging.info(f'Running...')
-    #builder.run()
-
-    KINDS = {
-        'value', 'valueset',
-        'op_assign', 'op_prefix', 'op_infix', 'op_postfix',
-        'op_typecast', 'op_typecheck',
-        'ref_var', 'ref_array', 'ref_field',
-        'call', 'new',
-    }
-    VALUES = {'value', 'valueset'}
-    CALLS = {'call', 'new'}
-    REFS = {'ref_var', 'ref_field', 'ref_array'}
-    CONDS = {'join', 'begin', 'end', 'case', 'catchjoin'}
-    def ignored(n):
-        return (len(n.inputs) == 1 and n.kind not in KINDS)
-    def getfeats(label, n):
-        if n.kind not in KINDS: return
-        if label.startswith('@'):
-            label = '@'
-        if n.kind in VALUES:
-            (_,t) = DFType.parse(n.ntype)
-            yield f'{label}:{n.kind}:{t}'
-        elif n.kind in REFS:
-            yield f'{label}:{n.kind}:{parserefname(n.ref)}'
-        elif n.kind in CALLS:
-            methods = n.data.split()
-            (_,name,_) = parsemethodname(methods[0])
-            yield f'{label}:{n.kind}:{name}'
-        else:
-            yield f'{label}:{n.kind}:{n.data}'
-        return
-    def visit(out, label0, n0, visited, n=0):
-        if n0 in visited: return
-        visited.add(n0)
-        if ignored(n0):
-            for (label1,n1) in n0.inputs.items():
-                if label1 == '#bypass': continue
-                if label1.startswith('_'): continue
-                visit(out, label0, n1, visited, n)
-        else:
-            for feat in getfeats(label0, n0):
-                out.add((n, feat))
-            n += 1
-            if n < maxpathlen:
-                for (label1,n1) in n0.inputs.items():
-                    if label1.startswith('_'): continue
-                    if label1 == '#bypass': continue
-                    if n1.kind == 'ref_array' and not label1: continue
-                    visit(out, label1, n1, visited, n)
-
-    # list all the methods and number of its uses. (being called)
-    for method in db.get_allmethods():
-        for node in method:
-            if not node.is_funcall(): continue
-            for func in node.data.split():
-                if func not in funargs: continue
-                for (label,arg) in funargs[func]:
-                    out = set()
-                    visit(out, arg, node.inputs[arg], set())
-                    print(label, sorted(out))
+        # list all the methods and number of its uses. (being called)
+        for method in db.get_allmethods():
+            for node in method:
+                if not node.is_funcall(): continue
+                for func in node.data.split():
+                    if func not in funargs: continue
+                    for (label,arg) in funargs[func]:
+                        out = set()
+                        visit(out, arg, node.inputs[arg], set())
+                        print(label, sorted(out))
     return 0
 
 if __name__ == '__main__': sys.exit(main(sys.argv))
