c70ad67abb10c250be60a49c86e8151735dd0783
Yusuke Shinyama <yusuke@shinyama.jp>
2022-04-10 21:33:53 +0900

    Updated: listvarpairs.py

--- tools/listvarpairs.py
+++ tools/listvarpairs.py
@@ -12,16 +12,18 @@ TYPEOPS = {'op_typecast', 'op_typecheck'}
 EXTOPS = {'op_infix', 'value'}
 
 IGNORED = {
-    None, 'ref_var', 'assign_var',
-    'receive', 'input', 'output', 'begin', 'end', 'repeat', 'catchjoin'}
+    None, 'receive', 'input', 'output', 'begin', 'end', 'repeat', 'catchjoin'
+}
 
 AUGMENTED = {
     'call', 'op_infix',
     'ref_array', 'ref_field',
-    'assign_array', 'assign_field'}
+    'assign_array', 'assign_field'
+}
 
-def is_ignored(label):
-    return (label.startswith('_') or label == '#bypass')
+def is_ignored(n, label):
+    return (label.startswith('_') or
+            (n.is_funcall() and not label.startswith('#arg')))
 
 def is_varref(ref):
     return (ref and ref[0] not in '#%!@')
@@ -61,18 +63,36 @@ def getfeat1(label, n):
         return f'{label},{n.kind},{n.data}'
 
 def getfeats(n0, label, n1):
-    if n1.kind in IGNORED: return []
-    if n1.kind == 'op_assign' and n1.data == '=': return []
+    if n1.kind in IGNORED: return None
+    if n1.kind == 'op_assign' and n1.data == '=': return None
     f1 = getfeat1(label, n1)
     feats = [f1]
     if n1.kind in AUGMENTED:
         for (k,n2) in n1.inputs.items():
-            if is_ignored(k): continue
+            if is_ignored(n2, k): continue
             if n2 is not n0 and k != label:
                 f2 = getfeatext(k, n2)
                 if f2 is not None:
                     feats.append(f1+'|'+f2)
     return feats
+
+def enumkeys(chain, key=None):
+    assert chain is not None
+    (_,n1) = chain.car
+    prev = chain.cdr
+    if prev is None:
+        yield key
+        return
+    (label,n0) = prev.car
+    feats = getfeats(n0, label, n1)
+    if feats is None:
+        for z in enumkeys(prev, key):
+            yield z
+    else:
+        for feat in feats:
+            for z in enumkeys(prev, Cons(feat, key)):
+                yield z
+    return
 
 
 # main
@@ -94,7 +114,6 @@ def main(argv):
     srcdb = None
     maxlen = 5
     maxchains = 32
-    grouping = False
     for (k, v) in opts:
         if k == '-d': level = logging.DEBUG
         elif k == '-o': output = v
@@ -103,7 +122,6 @@ def main(argv):
         elif k == '-c': encoding = v
         elif k == '-B': srcdb = SourceDB(v, encoding)
         elif k == '-m': maxlen = int(v)
-        elif k == '-G': grouping = True
     logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', level=level)
     if not args: return usage()
 
@@ -118,90 +136,70 @@ def main(argv):
         builder.load(path, fp)
 
     builder.run()
-    funcalls = sum( len(a) for a in builder.funcalls.values() )
-    logging.info(f'Read: {len(builder.srcmap)} sources, {funcalls} funcalls, {len(builder.vtxs)} IPVertexes')
+    logging.info(f'Read: {len(builder.srcmap)} sources, {len(builder.vtxs)} IPVertexes')
 
-    def trace(r, vtx0, srcs=None, chain=None):
-        if chain is not None and maxlen <= len(chain): return
-        node = vtx0.node
-        if srcs is not None and node in srcs: return
-        srcs = Cons(node, srcs)
-        #print('  '*level, node.name, node.kind, node.ref, node.data)
-        if node in r:
-            chains = r[node]
-        else:
-            chains = r[node] = []
-        if maxchains <= len(chains): return
-        chains.append(chain)
-        if chain is None:
-            n0 = None
-        else:
-            n0 = chain.car
-        for (label,vtx1,funcall) in vtx0.outputs:
-            if is_ignored(label): continue
-            n1 = vtx1.node
-            if n1.kind == 'call' and not label.startswith('#'): continue
-            feats = getfeats(n0, label, n1)
-            if feats:
-                for feat in feats:
-                    trace(r, vtx1, srcs, Cons((feat, n1), chain))
-            else:
-                trace(r, vtx1, srcs, chain)
+    def enumnodes(chains, label0, vtx0, prev=None):
+        n0 = vtx0.node
+        chain = Cons((label0,n0), prev)
+        if n0.kind in ASSIGNS:
+            assert prev is not None
+            if is_varref(n0.ref):
+                if len(chains) < maxchains:
+                    chains.append(chain)
+            return
+        if maxlen <= len(chain): return
+        for (label1,vtx1,_) in vtx0.outputs:
+            if is_ignored(n0, label1): continue
+            enumnodes(chains, label1, vtx1, chain)
         return
 
-    def put(node0, node1, chain):
-        n0 = node0.ref
-        n1 = node1.ref
-        if n0 is None or n1 is None or n0 == n1: return
-        nodes = [node0,node1] + [ n for (_,n) in chain ]
+    def put(nodes, key):
+        assert 2 <= len(nodes)
         def f(x):
             if x is None: return 'None'
             return '%d:%d:%d' % x
-        fp.write('+NODES %s\n' % (' '.join( f(builder.getsrc(n)) for n in nodes )))
-        fp.write('+PAIR %s %s %s\n' % (n0, n1, ' '.join( k for (k,_) in chain )))
+        fp.write('+CHAIN %s\n' % (' '.join( f(builder.getsrc(n)) for n in nodes )))
+        fp.write(f'+PAIR {nodes[0].ref} {nodes[-1].ref} {" ".join(key)}')
         if srcdb is not None:
             annot = SourceAnnot(srcdb)
-            chain.insert(0, (None,node0))
-            chain.append((None,node1))
-            for (i,(_,n)) in enumerate(chain):
+            for (i,n) in enumerate(nodes):
                 src = builder.getsrc(n, False)
                 if src is None: continue
                 (name,start,end) = src
                 annot.add(name, start, end, i)
             annot.show_text(fp)
+        fp.write('\n')
         return
 
     key2pair = {}
-    for vtx in builder:
-        node0 = vtx.node
+    for vtx0 in builder:
+        node0 = vtx0.node
         if node0.kind in REFS and is_varref(node0.ref):
-            r = {}
-            #print('trace', vtx.node)
-            trace(r, vtx)
-            for (node1,chains) in r.items():
-                if (node1.kind not in ASSIGNS or
-                    not is_varref(node1.ref)): continue
-                for chain in chains:
-                    if chain is None: continue
-                    a = list(chain)
-                    a.reverse()
-                    put(node0, node1, a)
-                    if grouping:
-                        key = tuple( k for (k,_) in a )
-                        if key in key2pair:
-                            p = key2pair[key]
-                        else:
-                            p = key2pair[key] = []
-                        p.append((node0,node1))
-    if grouping:
-        for (key,pairs) in key2pair.items():
-            if len(pairs) < 2: continue
-            fp.write('+GROUP %s\n' % ' '.join(key))
-            for (node0,node1) in pairs:
-                n0 = parserefname(node0.ref)
-                n1 = parserefname(node1.ref)
-                if n0 is None or n1 is None or n0 == n1: continue
-                fp.write('# %s %s\n' % (n0, n1))
+            chains = []
+            enumnodes(chains, None, vtx0)
+            for chain in chains:
+                nodes = [ n for (_,n) in chain ]
+                nodes.reverse()
+                ref0 = nodes[0].ref
+                ref1 = nodes[-1].ref
+                if ref0 is None or ref1 is None or ref0 == ref1: continue
+                for key in enumkeys(chain):
+                    if key is None: continue
+                    key = tuple(key)
+                    put(nodes, key)
+                    if key in key2pair:
+                        p = key2pair[key]
+                    else:
+                        p = key2pair[key] = []
+                    p.append((nodes[0].ref, nodes[-1].ref))
+
+    for (key,pairs) in key2pair.items():
+        if len(pairs) < 2: continue
+        fp.write(f'+GROUP {" ".join(key)}\n')
+        for (ref0,ref1) in pairs:
+            n0 = parserefname(ref0)
+            n1 = parserefname(ref1)
+            fp.write(f'+PAIR {parserefname(ref0)} {parserefname(ref1)}\n')
 
     if fp is not sys.stdout:
         fp.close()
